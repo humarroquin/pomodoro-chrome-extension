578f4eb95bd04e26d8e618734b03524d6b6400cd
Hugo <149089365+humarroquin@users.noreply.github.com>
2025-09-28 21:49:45 -0600

    added the date and time features

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const displayDate = document.getElementById("date");
+const displayTime = document.getElementById("time");
+
+// date
+const dateOptions = { month: "short", day: "numeric", weekday: "long" };
+const today = new Date().toLocaleDateString("en-US", dateOptions);
+displayDate.textContent = today;
+
+// time
+function updateTime() {
+  const timeOptions = { hour: "numeric", minute: "2-digit", hour12: true };
+  const time = new Date().toLocaleTimeString("en-US", timeOptions);
+  displayTime.textContent = time;
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
