f03322a14cd55312d3d8820413ab003744457f08
Hugo <149089365+humarroquin@users.noreply.github.com>
2025-10-11 20:05:26 -0600

    added functionality to the timer

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const displayDate = document.getElementById("date");
 const displayTime = document.getElementById("time");
 const btn = document.getElementById("show-tasks");
 const aside = document.getElementById("aside");
+// timer
+const timerUI = document.getElementById("timer");
+const startTimerBtn = document.getElementById("start-timer");
+let setTimeByUser = 25;
+let timeLeftInSeconds = setTimeByUser * 60;
+let isCountingDown = false;
+let timerIntervalID = null;
 
 // date
 const dateOptions = { month: "short", day: "numeric", weekday: "long" };
@@ -24,4 +31,32 @@ setInterval(updateTime, 1000);
 // show tasks
 btn.addEventListener("click", () => {
   aside.classList.toggle("show-tasks");
+});
+
+// timer
+const updateTimer = function () {
+  let minutes = Math.floor(timeLeftInSeconds / 60);
+  let seconds = timeLeftInSeconds % 60;
+  timerUI.textContent = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+};
+
+const startTimer = function () {
+  timerIntervalID = setInterval(() => {
+    timeLeftInSeconds--;
+    updateTimer();
+  }, 1000);
+};
+
+startTimerBtn.addEventListener("click", () => {
+  if (!isCountingDown) {
+    startTimer();
+    isCountingDown = true;
+  } else {
+    isCountingDown = false;
+    clearInterval(timerIntervalID);
+  }
+
+  startTimerBtn.textContent = !isCountingDown ? "Start" : "Stop";
 });
